7488b2865d4aecda258a74560e8838978406b12c
aomerk <aliomerkeser@gmail.com>
2021-05-23 15:45:29 +0200

    [feat] Register agent check command

--- cli/commands/agent/command.go
+++ cli/commands/agent/command.go
@@ -1,5 +1,12 @@
 package agent
 
+import (
+	`io`
+	`os`
+	
+	`github.com/spf13/cobra`
+)
+
 /*
 	Created by aomerk at 5/23/21 for project cli
 */
@@ -14,3 +21,27 @@ const ()
 // global variables (not cool) for this file
 var ()
 
+// NewAgentCommand serves the base command for controlling agent on host machine.
+//
+func NewAgentCommand() *cobra.Command {
+	var agentCommands = &cobra.Command{
+		Use:   "agent",
+		Short: "Control and manage agent",
+		Long:  `Install, Uninstall, Reset selected agent on current host machine`,
+		RunE:  ShowHelp(os.Stdout),
+	}
+	
+	agentCommands.AddCommand(
+		CheckCommand(),
+	)
+	return agentCommands
+}
+
+// ShowHelp shows the command help.
+func ShowHelp(err io.Writer) func(*cobra.Command, []string) error {
+	return func(cmd *cobra.Command, args []string) error {
+		cmd.SetOut(err)
+		cmd.HelpFunc()(cmd, args)
+		return nil
+	}
+}
