0b293ebf904cf4a3cca1e6b44c6d6fa9f5195e4e
aomerk <aliomerkeser@gmail.com>
2021-05-23 14:43:46 +0200

    [feat] [test] Implemented suspicion command
    - Support many flags
    - Support local config override
    - Added tests

--- cli/commands/suspicion/get_test.go
+++ cli/commands/suspicion/get_test.go
@@ -43,8 +43,8 @@ func TestGet(t *testing.T) {
 		t.Fatalf("unable to decode into map, %v", err)
 	}
 	type args struct {
-		dbConfig config.Database
-		args     QueryArgs
+		cliConfig config.Cli
+		args      QueryArgs
 	}
 	tests := []struct {
 		name    string
@@ -54,12 +54,12 @@ func TestGet(t *testing.T) {
 	}{
 		{
 			name:    "good credentials",
-			args:    args{dbConfig: cliConfig.Database, args: QueryArgs{Limit: 6}},
+			args:    args{cliConfig: cliConfig, args: QueryArgs{Limit: 6}},
 			wantErr: false,
 		}, {
 			name: "limit suspicion ids",
 			args: args{
-				dbConfig: cliConfig.Database,
+				cliConfig: cliConfig,
 				args: QueryArgs{
 					SuspicionIds: []string{
 						"15423ea9-d658-4cc2-ba21-37acd0f90c07", "40d91d87-3103-4ba9-9aae-55e3b05b1d70",
@@ -70,7 +70,7 @@ func TestGet(t *testing.T) {
 		}, {
 			name: "filter by suspect ids",
 			args: args{
-				dbConfig: cliConfig.Database,
+				cliConfig: cliConfig,
 				args: QueryArgs{
 					SuspectIds: []string{
 						"3981bb12-8ccc-4493-9884-9d8d46a2ca59", "3981bb12-8ccc-4493-9884-9d8d46a2ca59",
@@ -83,7 +83,7 @@ func TestGet(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(
 			tt.name, func(t *testing.T) {
-				got, err := Get(tt.args.dbConfig, tt.args.args)
+				got, err := Get(tt.args.cliConfig, tt.args.args)
 				if (err != nil) != tt.wantErr {
 					t.Errorf("Get() error = %v, wantErr %v", err, tt.wantErr)
 					return
