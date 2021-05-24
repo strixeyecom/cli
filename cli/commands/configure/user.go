dd8709d77904ee0d3746dd91a382c3e9c70cc0ba
aomerk <aliomerkeser@gmail.com>
2021-05-24 07:04:57 +0200

    [test][doc] Agent selection process improved
    - Added gif for selection process with mock data
    - Pass selection process only array of agents
    - testing agent selection process

--- cli/commands/configure/user.go
+++ cli/commands/configure/user.go
@@ -39,16 +39,16 @@ with a single StrixEye user's API Key.
 Authentication will be executed via this user's API Token' `,
 		RunE: selectUser,
 	}
-
+	
 	// Add subcommands
 	configureUserCommand.AddCommand()
-
+	
 	// Add local flags
 	configureUserCommand.Flags().StringP(
 		"set-api-key", "s", "",
 		`Set api key directly without cli prompt`,
 	)
-
+	
 	return configureUserCommand
 }
 
@@ -59,18 +59,18 @@ func promptUser() (string, error) {
 		userAPIKey string
 		prompt     *survey.Input
 	)
-
+	
 	// get api key from user input
 	prompt = &survey.Input{
-		Message: "Please enter your user api key\n>",
+		Message: "Please enter your User API Key\n>",
 	}
-
+	
 	// ask user prompt.
 	err = survey.AskOne(prompt, &userAPIKey)
 	if err != nil {
 		return userAPIKey, err
 	}
-
+	
 	return userAPIKey, nil
 }
 
@@ -81,13 +81,13 @@ func selectUser(cmd *cobra.Command, args []string) error {
 		userAPIKey string
 		cliConfig  cli.Cli
 	)
-
+	
 	// try to get from flags.
 	userAPIKey, err = cmd.Flags().GetString("set-api-key")
 	if err != nil {
 		return err
 	}
-
+	
 	// if flags aren't provided, let user choose from a select list.
 	if userAPIKey == "" {
 		userAPIKey, err = promptUser()
@@ -95,7 +95,7 @@ func selectUser(cmd *cobra.Command, args []string) error {
 			return err
 		}
 	}
-
+	
 	// try to authenticate user.
 	// no need to get the config file, viper supports setting values on the fly.
 	viper.Set("USER_API_TOKEN", userAPIKey)
@@ -103,19 +103,19 @@ func selectUser(cmd *cobra.Command, args []string) error {
 	if err != nil {
 		return err
 	}
-
+	
 	// ask api for authentication
 	err = authenticate.Authenticate(cliConfig)
 	if err != nil {
 		return errors.Wrap(err, "can not authenticate user with given credential.")
 	}
-
+	
 	// store edited viper config to file.
 	err = viper.WriteConfig()
 	if err != nil {
 		return errors.Wrap(err, "failed to save config. Do you have permissions on the filesystem? ")
 	}
-
+	
 	// Write success message.
 	color.Blue(
 		`Successfully authenticated.
@@ -123,6 +123,6 @@ You can start using StrixEye CLI after selecting a default agent if you haven't.
 Command for that is:`,
 	)
 	color.Green("\tstrixeye configure agent")
-
+	
 	return nil
 }
