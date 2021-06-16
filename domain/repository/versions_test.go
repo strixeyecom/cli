204739c1babb591a59a8dbbddebd065c279baf79
aomerk <aliomerkeser@gmail.com>
2021-06-16 10:38:36 +0200

    [feat] [test] Implemented Versions v2
    - Versions now have hashes and sizes
    
    - Started using reflect for parsing dynamic API Responses.
    
    - Adapted tests and golden tests

--- domain/repository/versions_test.go
+++ domain/repository/versions_test.go
@@ -1,6 +1,10 @@
 package repository
 
-import "testing"
+import (
+	`encoding/json`
+	`testing`
+	`time`
+)
 
 /*
 	Created by aomerk at 5/20/21 for project strixeye
@@ -18,14 +22,15 @@ var ()
 
 func TestVersions_Validate(t *testing.T) {
 	type fields struct {
-		ManagerVersion   string
-		DatabaseVersion  string
-		EngineVersion    string
-		ProfilerVersion  string
-		QueueVersion     string
-		SchedulerVersion string
-		SensorVersion    string
-	}
+	ManagerVersion   Version
+	DatabaseVersion  Version
+	EngineVersion    Version
+	ProfilerVersion  Version
+	QueueVersion     Version
+	SchedulerVersion Version
+	SensorVersion    Version
+	InstallVersion   Version
+}
 	tests := []struct {
 		name    string
 		fields  fields
@@ -34,36 +39,54 @@ func TestVersions_Validate(t *testing.T) {
 		{
 			name: "empty fields exist",
 			fields: fields{
-				ManagerVersion: "bah",
+				ManagerVersion: Version{Version: "v0.2.5-rc1.1-2505350", Size: 123456, Checksum: "checksum"},
 			},
 			wantErr: true,
 		}, {
 			name: "version with commit hash",
 			fields: fields{
-				ManagerVersion:   "v0.2.5-rc1.1-2505350",
-				DatabaseVersion:  "v0.2.5-rc1.1-2505350",
-				EngineVersion:    "v0.2.5-rc1.1-2505350",
-				ProfilerVersion:  "v0.2.5-rc1.1-2505350",
-				QueueVersion:     "v0.2.5-rc1.1-2505350",
-				SchedulerVersion: "v0.2.5-rc1.1-2505350",
-				SensorVersion:    "v0.2.5-rc1.1-2505350",
+				ManagerVersion: Version{
+					Version: "v0.2.5-rc1.1", Size: 123456, Checksum: "checksum",
+				},
+				DatabaseVersion: Version{
+					Version: "v0.2.5-rc1.1", Size: 123456, Checksum: "checksum",
+				},
+				EngineVersion: Version{
+					Version: "v0.2.5-rc1.1", Size: 123456, Checksum: "checksum",
+				},
+				ProfilerVersion: Version{
+					Version: "v0.2.5-rc1.1", Size: 123456, Checksum: "checksum",
+				},
+				QueueVersion: Version{
+					Version: "v0.2.5-rc1.1", Size: 123456, Checksum: "checksum",
+				},
+				SchedulerVersion: Version{
+					Version: "v0.2.5-rc1.1", Size: 123456, Checksum: "checksum",
+				},
+				SensorVersion: Version{
+					Version: "v0.2.5-rc1.1", Size: 123456, Checksum: "checksum",
+				},
+				InstallVersion: Version{
+					Version: "v0.2.5-rc1.1", Size: 123456, Checksum: "checksum",
+				},
 			},
 			wantErr: false,
 		},
 	}
 	for i := range tests {
 		tt := tests[i]
-
+		
 		t.Run(
 			tt.name, func(t *testing.T) {
 				m := Versions{
-					ManagerVersion:   tt.fields.ManagerVersion,
-					DatabaseVersion:  tt.fields.DatabaseVersion,
-					EngineVersion:    tt.fields.EngineVersion,
-					ProfilerVersion:  tt.fields.ProfilerVersion,
-					QueueVersion:     tt.fields.QueueVersion,
-					SchedulerVersion: tt.fields.SchedulerVersion,
-					SensorVersion:    tt.fields.SensorVersion,
+					Manager:   tt.fields.ManagerVersion,
+					Database:  tt.fields.DatabaseVersion,
+					Engine:    tt.fields.EngineVersion,
+					Profiler:  tt.fields.ProfilerVersion,
+					Queue:     tt.fields.QueueVersion,
+					Scheduler: tt.fields.SchedulerVersion,
+					Sensor:    tt.fields.SensorVersion,
+					Installer:    tt.fields.InstallVersion,
 				}
 				if err := m.Validate(); (err != nil) != tt.wantErr {
 					t.Errorf("Validate() error = %v, wantErr %v", err, tt.wantErr)
@@ -73,7 +96,7 @@ func TestVersions_Validate(t *testing.T) {
 	}
 }
 
-func TestVersions_FromRawAPIResponse(t *testing.T) {
+func TestVersions_ToVersions(t *testing.T) {
 	type fields struct {
 		ManagerVersion   string
 		DatabaseVersion  string
@@ -103,12 +126,65 @@ func TestVersions_FromRawAPIResponse(t *testing.T) {
 	}
 	for i := range tests {
 		tt := tests[i]
-
+		
 		t.Run(
 			tt.name, func(t *testing.T) {
-				v := Versions{}
-				if err := v.FromRawAPIResponse(tt.args.rawData); (err != nil) != tt.wantErr {
+				v := APIVersionsMessage{}
+				err := json.Unmarshal(tt.args.rawData, &v)
+				if err != nil {
+					t.Fatal(err)
+				}
+				if _, err := v.ToVersions(); (err != nil) != tt.wantErr {
 					t.Errorf("FromRawAPIResponse() error = %v, wantErr %v", err, tt.wantErr)
+				}
+			},
+		)
+	}
+}
+
+func Test_decode(t *testing.T) {
+	type args struct {
+		s APIVersionsMessage
+		i interface{}
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "success",
+			args: args{
+				i: &Versions{},
+				s: APIVersionsMessage{
+					Data: []struct {
+						Key       string     `json:"key"`
+						Value     Version    `json:"value"`
+						CreatedAt *time.Time `json:"created_at"`
+						UpdatedAt *time.Time `json:"updated_at"`
+						DeletedAt *time.Time `json:"deleted_at"`
+					}{
+						{
+							Key: "Manager", Value: Version{
+							Version: "test-version", Size: 12345,
+							Checksum: "test-checksum",
+						},
+						}, {
+							Key: "Installer", Value: Version{
+								Version: "test-installer-version", Size: 54322,
+								Checksum: "test-checksum",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				if err := decode(tt.args.s, tt.args.i); (err != nil) != tt.wantErr {
+					t.Errorf("decode() error = %v, wantErr %v", err, tt.wantErr)
 				}
 			},
 		)
@@ -120,73 +196,102 @@ const (
     "status": "ok",
     "data": [
         {
-            "key": "installer_version",
-            "value": "v0.1.3-rc1-8deb131",
-            "created_at": null,
-            "updated_at": null,
-            "deleted_at": null
-        },
-        {
-            "key": "sensor_version",
-            "value": "staging",
-            "created_at": null,
-            "updated_at": null,
-            "deleted_at": null
-        },
-        {
             "key": "dashboard_version",
-            "value": "staging",
-            "created_at": null,
-            "updated_at": null,
+            "value": {
+                "version": "staging",
+                "checksum": "hash",
+                "size": 123456
+            },
+            "created_at": "2021-06-14T17:07:02.000000Z",
+            "updated_at": "2021-06-14T17:07:02.000000Z",
+            "deleted_at": null
+        },
+        {
+            "key": "installer_version",
+            "value": {
+                "version": "v0.2.5-rc1.1-2505350",
+                "checksum": "hash",
+                "size": 123456
+            },
+            "created_at": "2021-06-14T17:07:02.000000Z",
+            "updated_at": "2021-06-14T17:07:02.000000Z",
             "deleted_at": null
         },
         {
             "key": "scheduler_version",
-            "value": "v0.1.2-rc2-0e4ef28",
-            "created_at": null,
-            "updated_at": "2021-05-19T10:21:41.000000Z",
+            "value": {
+                "version": "staging",
+                "checksum": "hash",
+                "size": 123456
+            },
+            "created_at": "2021-06-14T17:07:02.000000Z",
+            "updated_at": "2021-06-14T17:07:02.000000Z",
             "deleted_at": null
         },
         {
             "key": "queue_version",
-            "value": "staging",
-            "created_at": null,
-            "updated_at": null,
+            "value": {
+                "version": "staging",
+                "checksum": "hash",
+                "size": 123456
+            },
+            "created_at": "2021-06-14T17:07:02.000000Z",
+            "updated_at": "2021-06-14T17:07:02.000000Z",
             "deleted_at": null
         },
         {
             "key": "profiler_version",
-            "value": "v0.2.4-rc6-d39fa3f",
-            "created_at": null,
-            "updated_at": "2021-05-19T10:06:44.000000Z",
+            "value": {
+                "version": "staging",
+                "checksum": "hash",
+                "size": 123456
+            },
+            "created_at": "2021-06-14T17:07:02.000000Z",
+            "updated_at": "2021-06-14T17:07:02.000000Z",
             "deleted_at": null
         },
         {
             "key": "database_version",
-            "value": "staging",
-            "created_at": null,
-            "updated_at": null,
+            "value": {
+                "version": "staging",
+                "checksum": "hash",
+                "size": 123456
+            },
+            "created_at": "2021-06-14T17:07:02.000000Z",
+            "updated_at": "2021-06-14T17:07:02.000000Z",
+            "deleted_at": null
+        },
+        {
+            "key": "sensor_version",
+            "value": {
+                "version": "staging",
+                "checksum": "hash",
+                "size": 123456
+            },
+            "created_at": "2021-06-14T17:07:02.000000Z",
+            "updated_at": "2021-06-14T17:07:02.000000Z",
             "deleted_at": null
         },
         {
             "key": "engine_version",
-            "value": "v0.2.9-rc1.3-b81395e",
-            "created_at": null,
-            "updated_at": "2021-05-19T09:40:09.000000Z",
+            "value": {
+                "version": "staging",
+                "checksum": "hash",
+                "size": 111
+            },
+            "created_at": "2021-06-14T17:07:02.000000Z",
+            "updated_at": "2021-06-14T17:07:02.000000Z",
             "deleted_at": null
         },
         {
             "key": "manager_version",
-            "value": "0.2.5-rc1.1",
-            "created_at": null,
-            "updated_at": null,
-            "deleted_at": null
-        },
-        {
-            "key": "backoffice_version",
-            "value": "staging",
-            "created_at": null,
-            "updated_at": null,
+            "value": {
+                "version": "v0.2.5-rc1.1-f2c7578",
+                "size": 111,
+                "hash": "111"
+            },
+            "created_at": "2021-06-14T17:07:02.000000Z",
+            "updated_at": "2021-06-15T18:15:57.000000Z",
             "deleted_at": null
         }
     ]
