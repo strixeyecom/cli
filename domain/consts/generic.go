bd0e0e1575cf8a0da66fa2d956d738b188fe198b
Ali Keser <aliomer@strixeye.com>
2021-08-09 15:06:39 +0200

    [refactor] Project structure improvements

---
+++ domain/consts/generic.go
@@ -0,0 +1,19 @@
+package consts
+
+/*
+	Created by aomerk at 8/9/21 for project strixeye
+*/
+
+/*
+	INSERT FILE DESCRIPTION HERE
+*/
+
+
+// global constants for file
+const (
+
+)
+
+// global variables (not cool) for this file
+var ()
+
